3bde2f09b2af49e03090f9ca8e52600c1474c7e2
Josiah Sayles <Josiah.Sayles@gmail.com>
2025-11-11 18:06:40 -0700

    added home-page and styled it still workshopping styling and layout, subject to change.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { Routes, Route } from "react-router";
 import "./App.css";
 import Layout from "./Layout";
 import ProjectIdeas from "./components/ProjectIdeas/ProjectIdeas";
-
-
+import HomePage from "./components/HomePage/homePage";
 function App() {
   return (
     <>
       <Routes>
         <Route element={<Layout />}>
+          <Route path="home" index element={<HomePage />} />
           <Route path="projects" element={<ProjectIdeas />} />
           <Route path="*" element={<h1>404</h1>} />
         </Route>
